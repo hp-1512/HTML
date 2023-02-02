a36aeccbd559c524025d74c947dc200f5fefc5e6
Harshkumar A Patel <harshkumarpatel1512@gmail.com>
2023-02-02 09:28:24 +0530

    assignments of html

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+const targetDiv = document.getElementById("side-bar");
+const btn = document.getElementById("menu");
+btn.onclick = function () {
+    if (targetDiv.style.display !== "none") {
+        targetDiv.style.display = "none";
+    } else {
+        targetDiv.style.display = "block";
+    }
+};
